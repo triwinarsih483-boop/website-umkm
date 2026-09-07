aeb849ebb703bf2dfbe2cde032968ab741ecb9ae
triwinarsih483.boop <triwinarsih483@smk.belajar.id>
2026-09-07 11:09:38 +0700

    feat: buat lading page awal UMKM

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const promoButton = document.querySelector("#promoButto");
+
+promoButton.addEventListener("click", () => {
+
+    promoButton.textContent = "promo: beli 2 gratis tester!" ;
+    console.log("promo kopi nusa berhasil ditambahkan.");
+});
